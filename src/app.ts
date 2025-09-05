048c862a55a1d1725a2223acc755c97a25bc3f0c
adityaraghuvanshi <68696650+adityaraghuvanshi@users.noreply.github.com>
2025-09-05 22:30:01 +0530

    Connected to mongo server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import express, { NextFunction, Request, Response } from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const mongoServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CONNECTURL as string, {
+            dbName: "Create, Read, Update, Delete Database"
+        });
+        console.log("MongoDB server connected.")
+    }
+    catch (error) {
+        throw new Error("Not able to start mongo server.");
+    }
+}
+mongoServer();
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.json({ message: "Server is here!" });
